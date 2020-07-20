6326f2adbb42353414a2ab0d030bed605d3ef88c
Kodai Yamamoto <kyama8209@gmail.com>
2020-07-20 16:18:32 +0900

    update post_new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,23 @@
 class PostsController < ApplicationController
   def index
-    @post = Post.all
+    @posts = Post.all
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
+  private
+  def post_params
+    params.require(:post).permit(:title, :text).merge(user_id: current_user.id)
   end
 end
