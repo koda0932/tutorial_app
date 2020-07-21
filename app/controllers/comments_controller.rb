41efe3769bf8b5548c5c64dced0a937af2687491
Kodai Yamamoto <kyama8209@gmail.com>
2020-07-21 15:22:32 +0900

    add gem fontawesome

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,16 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
-    redirect_to "/posts/#{comment.post.id}"
+    @comment = Comment.create(comment_params)
+    redirect_to "/posts/#{@comment.post.id}"
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id]).destroy
+    redirect_to "/posts/#{@comment.post.id}"
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:content).merge(user_id: current_user.id, post_id: params[:post_id])
+    params.require(:comment).permit(:content, :reply).merge(user_id: current_user.id, post_id: params[:post_id])
   end
 end
